96730834e4ef7fa67cba40cad88db84041f9a27b
Marcin Żołna <marcin.zolna@brainly.com>
2015-01-14 21:43:52 +0100

    - Page Object open action

--- lib/ServicePageObject.php
+++ lib/ServicePageObject.php
@@ -5,9 +5,27 @@ namespace lib\ServicePageObject;
 class ServicePageObject
 {
   protected $webdriver;
+  protected $url;
   
-  public function __construct($webdriver)
+  public function __construct($webdriver, $url)
   {
     $this->webdriver = $webdriver;
+    $this->url = $url;
+  }
+  
+  public function getWebdriver()
+  {
+    return $this->webdriver;
+  }
+  
+  public function getUrl()
+  {
+    return $this->url;
+  }
+  
+  public function open()
+  {
+    $this->webdriver->get($this->url);
+    return $this;
   }
 }
