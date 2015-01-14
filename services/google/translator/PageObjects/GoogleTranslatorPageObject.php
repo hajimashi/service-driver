6ed2ea4ff36f59e77bea8f7e0b91305068620015
Marcin Żołna <marcin.zolna@brainly.com>
2015-01-15 00:40:08 +0100

    - [gt] Page object

--- services/google/translator/PageObjects/GoogleTranslatorPageObject.php
+++ services/google/translator/PageObjects/GoogleTranslatorPageObject.php
@@ -6,7 +6,9 @@ use lib\ServicePageObject;
 
 class GoogleTranslatorPageObject extends ServicePageObject
 {
-  public function selectLanguageFrom($language)
+  protected $url = "https://translate.google.pl/";
+  
+  public function selectSourceLanguage($language)
   {
     $this->selectFromDropDownMenu(
       WebDriverBy::id("gt-sl-gms"),
@@ -16,7 +18,7 @@ class GoogleTranslatorPageObject extends ServicePageObject
     );
   }
   
-  public function selectLanguageTo()
+  public function selectTranslationLanguage()
   {
     $this->selectFromDropDownMenu(
       WebDriverBy::id("gt-tl-gms"),
@@ -37,14 +39,30 @@ class GoogleTranslatorPageObject extends ServicePageObject
   
   public function getTranslation()
   {
+    return $this->webdriver
+      ->findElement(WebDriverBy::id("result_box"))
+      ->getText();
   }
   
   public function clickSwitchLanguageButton()
   {
+    $this->webdriver
+      ->findElement(WebDriverBy::id("gt-swap"))
+      ->click();
   }
   
   public function clearTranslateFromTextarea()
   {
+    $this->webdriver
+      ->findElement(WebDriverBy::id("gt-clear"))
+      ->click();
+  }
+  
+  public function insertSourceText($text)
+  {
+    $this->webdriver
+      ->findElement(WebDriverBy::id("source"))
+      ->sendKeys($text);
   }
   
   protected function selectFromDropDownMenu($menuSelector, $elementSelector)
