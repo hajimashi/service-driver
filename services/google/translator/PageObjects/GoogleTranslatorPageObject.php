ac5ae61813219f0043469ca589b1c3a9b42ee5fc
Marcin Żołna <marcin.zolna@brainly.com>
2015-01-14 20:32:05 +0100

    - google translator page object definition

---
+++ services/google/translator/PageObjects/GoogleTranslatorPageObject.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class GoogleTranslatorPageObject
+{
+  
+  public function selectLanguageFrom
+  {
+  
+  }
+  
+  public function selectLanguageTo
+  {
+  }
+  
+  public function clickTranslateButton
+  {
+  }
+  
+  public function getTranslation
+  {
+  }
+  
+  public function clickSwitchLanguageButton
+  {
+  }
+  
+  public function clearTranslateFromTextarea
+  {
+  }
+}
